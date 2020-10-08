ee0404945d94368e00206509e19ebbcaebc85c15
phathung2001 <phathung2001@gmail.com>
2020-10-08 09:53:38 +0700

    Add ax+by=c to EQN mode, simplify COMP mode

--- Modes.h
+++ Modes.h
@@ -1,12 +1,5 @@
 #include "Input.h"
 
-// Modes
-#define COMPMODE 1
-#define EQNMODE 2
-#define INTGRLMODE 3
-#define ABOUTMODE 4
-#define MENUMODE 0
-
 // Function prototypes
 bool modeKeyCheck (String key); 
 void eqnMode();
@@ -14,7 +7,6 @@ void compMode ();
 void menuMode();
 void aboutMode();
 
-byte mode = MENUMODE;
 int currentDisplayingChars = 0;
 int scrollIndex = 0;
 
@@ -33,17 +25,14 @@ void menuMode() {
         }
         
         if (key == "1") {
-            mode = COMPMODE;
             compMode();
             printMenu = true;
         }
         else if (key == "2") {
-            mode = EQNMODE;
             eqnMode();  
             printMenu = true;
         }
         else if (key == "4") {
-            mode = ABOUTMODE;
             aboutMode();
             printMenu = true;
         }
@@ -94,7 +83,7 @@ void eqnMode() {
     lcd.clear();
     lcd.print(F("1.ax^2+bx+c=0"));
     lcd.setCursor(0, 1);
-    lcd.print(F("2.ax+by=0"));
+    lcd.print(F("2.ax+by=c"));
 
     do {
         key = getKeyStr();
@@ -107,9 +96,11 @@ void eqnMode() {
     if (key == "1") {
         Serial.println(F("Quadratic eq chosen"));
         lcd.clear();
+        lcd.blink();
         float a = scanCoefficient("a=");
         float b = scanCoefficient("b=");
         float c = scanCoefficient("c=");
+        lcd.noBlink();
 
         float delta = b*b-4*a*c;
 
@@ -126,107 +117,61 @@ void eqnMode() {
         else if (delta < 0) {
             lcd.clear();
             lcd.print("No solution");
+            pressAnyKey();
         }
     }
     else if (key == "2") {
-        
+        Serial.println(F("Sys of eqs 2 vars chosen"));
+        lcd.clear();
+        lcd.blink();
+
+        float a1 = scanCoefficient("[a1x+b1y=c1] a1=");
+        float b1 = scanCoefficient("[a1x+b1y=c1] b1=");
+        float c1 = scanCoefficient("[a1x+b1y=c1] c1=");
+        float a2 = scanCoefficient("[a2x+b2y=c2] a2=");
+        float b2 = scanCoefficient("[a2x+b2y=c2] b2=");
+        float c2 = scanCoefficient("[a2x+b2y=c2] c2=");
+
+        float d = a1*b2 - a2*b1;
+        float dx = c1*b2 - c2*b1;
+        float dy = a1*c2 - a2*c1;
+
+        sprintVariable<float>("d", d);
+        sprintVariable<float>("dx", dx);
+        sprintVariable<float>("dy", dy);
+
+        if (d == 0 && (dx != 0 || dy != 0)) {
+            lcd.clear();
+            lcd.print("No solution");
+            pressAnyKey();
+        }
+        else if (d == 0 && dx == 0 && dy == 0) {
+            lcd.clear();
+            lcd.print("Infnt solutions");
+            pressAnyKey();
+        }
+        else {
+            float x = dx/d;
+            float y = dy/d;
+            printSolution("x=", x);
+            printSolution("y=", y);            
+        }
     }
-    pressAnyKey();
 }
 
 void compMode () {
     lcd.clear();
-    lcd.blink();
+    bool menuflag = false;
+    float result = 0;
 
     Serial.println(F("Get in function compMode:"));
     
-    String tokens[max_size];
-    int ti = 0;
-
-    while (true) {
-        String key = getKeyStr();
-        
-        if (key != None) {
-            byte keyTokenType = getTokenType(key);
-            byte currTokenType = getTokenType(tokens[ti]);
-            Serial.print(key);
-            
-            if (modeKeyCheck(key)) return;
-            
-            if (key == "=") {
-                Serial.println();
-                sprintMemoryUsage(F("Free mem when pressed =: "));
-                
-                sstack postfix;
-                ConvertToPostfix(tokens, ti + 1, postfix);
-                
-                if (!errflag) {
-                    sprintArr(postfix.data, postfix.index + 1);
-                       
-                    float result = PostfixEvaluate(postfix);
-                    int precision = 6;
-                    Serial.println();
-                    Serial.println(result, precision);
-                     
-                    lcd.clear();
-                    lcd.home();
-                    lcd.noBlink();
-                    lcd.print(F("Result:"));
-                    lcd.setCursor(0, 1);
-                    lcd.print(result, precision);
-                }
-                else {
-                    // After handling
-                    errflag = false;
-                }
-                // Reset
-                ti = 0;
-                tokens[ti] = "";
-                continue;
-            }
-            else if (key == "<") {
-                if (currentDisplayingChars == 16) scrollIndex--;
-                printExpression(tokens, scrollIndex + 1);
-                continue;
-            }
-            else if (key == ">") {
-                if (scrollIndex < ti) scrollIndex++;
-                printExpression(tokens, scrollIndex + 1);
-                continue;
-            }
-            else if (key == "!") {
-                if (currTokenType == NUM && tokens[ti] != "Ans" && tokens[ti] != "pi") {
-                    tokens[ti].remove(tokens[ti].length() - 1);
-                    printExpression(tokens, ti + 1);
-                }
-                else {
-                    tokens[ti] = "";
-                    ti--;
-                }
-                
-                Serial.print(F("\nAfter remove: "));
-                sprintArr(tokens, ti + 1, NO_NEWLINE);
-                
-                printExpression(tokens, ti + 1);
-                continue;
-            }
-            
-            // Bound checking
-            if (ti >= max_size) continue;
-            if (ti < 0) ti = 0;
-            
-            if (currTokenType == UNK) {
-                tokens[ti] = key;
-            }
-            else if (keyTokenType == currTokenType && keyTokenType == NUM) {
-                tokens[ti] += key;
-            }
-            else {
-                ti++;
-                tokens[ti] = key;
-            }
-            scrollIndex = ti;
-            printExpression(tokens, ti + 1);
+    do {
+        lcd.blink();
+        result = scanExpression(menuflag);
+        if (!menuflag) {
+            lcd.noBlink();
+            printSolution("Result:", result);
         }
-    }
+    } while (!menuflag);
 }
