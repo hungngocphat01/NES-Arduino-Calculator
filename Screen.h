40a528fee0f6096722225b0db39210570c312bcd
phathung2001 <phathung2001@gmail.com>
2020-10-08 09:53:51 +0700

    Add function to clear a specific line

--- Screen.h
+++ Screen.h
@@ -3,7 +3,15 @@
 
 LiquidCrystal_I2C lcd(0x27,16,2);
 
-void printExpression(String* tokens, int n, int scroll = 0) {    
+void lcdClrLine(byte line) {
+    lcd.setCursor(0, line);
+    for (int i = 0; i < BUFFERSIZE; i++) {
+        lcd.print(" ");
+    }
+    lcd.setCursor(0, line);
+}
+
+void printExpression(String* tokens, int n, byte line = 0) {    
     int totalLength = 0;
     char buffer[BUFFERSIZE + 1];
     int k = BUFFERSIZE;
@@ -26,7 +34,16 @@ void printExpression(String* tokens, int n, int scroll = 0) {
     }
     buffer[len] = '\0';
         
-    lcd.clear();
+    lcdClrLine(line);
+    lcd.setCursor(0, line);
     lcd.blink();
     lcd.print(buffer);
+}
+
+template <typename T>
+void sprintVariable(String varname, T value) {
+    Serial.print(varname);
+    Serial.print(": ");
+    Serial.print(value);
+    Serial.println();
 }
